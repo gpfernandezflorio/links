d2ef1e9d19238728b19e82852a4eb4cedabfbba9
Gonzalo Pablo Fernandez <gpfernandez@dc.uba.ar>
2023-07-22 09:11:53 -0300

    Primer commit

---
+++ links.js
@@ -0,0 +1,15 @@
+const redirigir = {
+  figus:"https://gpfernandezflorio.github.io/milanator/?juego=figus0&toolbox=off"
+};
+
+const argumentoURL = function(key) {
+  let valor = location.search.match(new RegExp('[?&]' + key + '=([^&]+)'));
+  if (valor) {
+    return decodeURIComponent(valor[1].replace(/\+/g, '%20'));
+  }
+}
+
+let destino = argumentoURL('dst');
+if (destino && destino in redirigir) {
+  window.location.replace(redirigir.destino);
+}
